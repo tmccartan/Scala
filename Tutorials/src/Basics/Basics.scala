c7a6dbb5871bcb4f960ffa323d29137746372b5c
tmccartanIOS <rowing28>
2015-01-11 17:07:41 +0000

    Objects, cases, case classes
    more tutorial items

--- Tutorials/src/Basics/Basics.scala
+++ Tutorials/src/Basics/Basics.scala
@@ -27,5 +27,26 @@ class HelloWorld
   {
      n.contains(m)
   }
+  
+  def increaseSize(o:Any)={
+    o match {
+      case i:Int if i< 0 => -1
+      case i : Int => i+1
+      case d:Double if d < 0.0 => d- 0.1
+      case d:Double => d + 0.1
+      case _ => "Not a number"
+    }
+  }
+  
 }
+
+case class Computer(cpu:String, mem:String)
+
+object HelloWorld{
+  def apply()={
+    new HelloWorld();
+  }
+}
+
+
 
